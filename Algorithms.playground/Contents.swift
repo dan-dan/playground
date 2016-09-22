965af58a72b823800a6d451bf1634d0cce27d236
dan-dan <danielhc2@gmail.com>
2016-09-21 21:13:24 -0700

    cleared LowArray

--- Algorithms.playground/Contents.swift
+++ Algorithms.playground/Contents.swift
@@ -4,37 +4,3 @@ import UIKit
 
 var str = "Hello, playground"
 
-
-// Walking Through Chapter 2.  Create "Basic" Array
-
-struct lowArray<T: Comparable> {
-    
-    private var array = [T]()
-    
-    init(array: [T]) {
-        self.array = array;
-    }
-    
-    /**
-     - Parameters:
-        - index: Index to set the value
-        - value: value to set for the element
-
-    */
-    
-    public mutating func setElem(index: Int, value: T) {
-        self.array[index] = value;
-    }
-    
-    
-    /**
-     - Parameters:
-        - index: Index to return the value
-    - Returns: element requested
-     */
-    
-    public  func getElem(index: Int) -> T {
-        return self.array[index];
-    }
-    
-}
